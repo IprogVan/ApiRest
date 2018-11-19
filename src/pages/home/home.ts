e32c1bd5a11a3d3b6adc3cae8c71bb471fab3a4d
unknown <DAM207@ACARBALLEIRA.LOCAL>
2018-11-19 12:33:11 +0100

    a

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Proveedor1Provider} from '../../providers/proveedor1/proveedor1';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public proveedor:Proveedor1Provider) {
 
+  }
+
+  usuarios:any;
+
+  ionViewDidLoad(){
+
+    this.proveedor.ObtenerDatos().subscribe(
+      (data)=>{this.usuarios=data;},
+      (error)=>{console.log(error);}
+    )
   }
 
 }
